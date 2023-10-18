ad03020acd55b73fa9abd988a1ae1a0a75438186
Nicolas RIVIERE <hello@undefined.fr>
2023-10-18 19:11:50 +0200

    edit version

--- linky.php
+++ linky.php
@@ -10,7 +10,7 @@
   Plugin Name: Linky
   Plugin URI: https://www.undefined.fr
   Description: Create & manage link’s hub for your social profile directly in your websites
-  Version: 1.4.5
+  Version: 1.4.6
   Author Name: Nicolas RIVIERE (hello@undefined.fr)
   Author: Nicolas RIVIERE (Undefined)
   Domain Path: /languages
